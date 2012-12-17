ac6f5fb13a7e3ec582a8f19bf67d392961a53035
test <sriram.kasthuriuk@gmail.com>
2012-12-17 15:49:22 +0000

    scala-sample updates

--- scala-filehandling/src/filehandling/writetofile.scala
+++
@@ -1,9 +0,0 @@
-package filehandling
-import java.io._
-object WriteToFile {
-   def main(args: Array[String]) {
-      val writer = new PrintWriter(new File("test.txt" ))
-      writer.write("Hello Scala")
-      writer.close()
-   }
-}
